e8fac7ece36c001db66c0a1854bf7164efa3f626
kukichek <tavi4ham@gmail.com>
2019-10-19 12:28:08 +0300

    Added version of Lab6 which doesn't process letters and throws exceptions

--- Lab6/src/FrontZerosDeleter.java
+++ Lab6/src/FrontZerosDeleter.java
@@ -1,54 +1,55 @@
 import java.util.StringTokenizer;
 
 public class FrontZerosDeleter {
-    private static class DeleterFromString {
-        String numbers = new String("123456789");
-        boolean isPreviousDelimiterPoint = false;
-
-        private String deleteZeroesFromToken(String curToken) {
-            if (curToken.length() == 1) {
-                isPreviousDelimiterPoint = (curToken.charAt(0) == '.' ? true : false);
-                return curToken;
-            }
-
-            if (isPreviousDelimiterPoint || (curToken.charAt(0) != '0')) {
-                return curToken;
-            }
-
-            int i = 0;
-            while(i < curToken.length() && (curToken.charAt(i) == '0')) {
-                i++;
-            }
-
-            if (i == curToken.length()) {
-                return "0";
-            } else {
-                return curToken.substring(i);
-            }
+    private static String deleteZerosFromToken(String curToken) throws NumberFormatException {
+        if (curToken.length() == 1) {
+            return curToken;
         }
 
-        private String deleteZerosFromString(String curString) {
-            StringBuilder clearString = new StringBuilder();
-            StringTokenizer tokenizer = new StringTokenizer(curString,
-                    " .,;:[]{}()!?\\/abcdefghijklmnopqrstuvwxyzABCDEFHIJKLMOPQRSTUVWXYZ", true);
+        Double doubleStr = new Double(curToken);
 
-            while(tokenizer.hasMoreTokens()) {
-                clearString.append(deleteZeroesFromToken(tokenizer.nextToken()));
-            }
-
-            return clearString.toString();
+        if ((curToken.charAt(0) != '0')) {
+            return curToken;
         }
-    };
+
+        int i = 0;
+        while(i < curToken.length() && (curToken.charAt(i) == '0')) {
+            i++;
+        }
+
+        if (i == curToken.length()) {
+            return "0";
+        } else {
+            if (curToken.charAt(i) == '.') {
+                return curToken.substring(i - 1);
+            }
+            return curToken.substring(i);
+        }
+    }
+
+    private static String deleteZerosFromString(String curString) {
+        StringBuilder clearString = new StringBuilder();
+        StringTokenizer tokenizer = new StringTokenizer(curString,
+                " ,;:[]{}()!?\\/", true);
+
+        while(tokenizer.hasMoreTokens()) {
+            clearString.append(deleteZerosFromToken(tokenizer.nextToken()));
+        }
+
+        return clearString.toString();
+    }
 
     public static void main(String[] args) {
-        DeleterFromString deleter = new DeleterFromString();
+        try {
+            for (int i = 0; i < args.length; ++i) {
+                args[i] = deleteZerosFromString(args[i]);
 
-        System.out.println(args[0]);
+                System.out.println(args[i]);
+            }
+        }
 
-        for (int i = 0; i < args.length; ++i) {
-            args[i] = deleter.deleteZerosFromString(args[i]);
-
-            System.out.println(args[i]);
+        catch(NumberFormatException error) {
+            System.err.println(error.getMessage());
         }
     }
 }
